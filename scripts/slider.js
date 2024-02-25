13e3feb34c8fdb59a82e95d3b043e0ee2e2c5236
Daniil Sokolov <63972347+daniilsokolovv@users.noreply.github.com>
2024-02-25 18:12:22 +0300

    Добавил слайдер

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,6 +1,15 @@
-$(document).ready(function(){
-	$(".slider").bxSlider(
-		// pager: false,
-		// controls: false
-	);
-});
+	const slider = $(".slider").bxSlider({
+		pager:false,
+		controls:false,
+		speed: 400
+	});
+
+	$('.slider-arrow__icon--left').click((e) => {
+		e.preventDefault();
+		slider.goToPrevSlide();
+	});
+
+	$('.slider-arrow__icon--right').click((e) => {
+		e.preventDefault();
+		slider.goToNextSlide();
+	});
